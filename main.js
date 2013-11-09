cd22d5d819db375137ec692f373f125579bf7bf7
Parker Snell <parkersnell@gmail.com>
2013-11-08 16:18:34 -0800

    add config system

--- main.js
+++ main.js
@@ -4,23 +4,65 @@ var http = require('http'),
     fs = require('fs'),
     gameserver = require('./server/gameserver');
 
-// Default port: 3000, unless PORT env is set, or -port arg is given.
-var port = 3000;
-
-if (process.env.PORT) {
-  port = process.env.PORT;
+var config;
+try {
+  config = require('./config.json');
+  console.log('found config.json');
 }
+catch (e) {
+  console.log('couldn\'t find config.json - using defaults');
+  config = {};
+}
+
+// Allow command line config in the form -c[option]=[value].
+// This overrides config.json.
 process.argv.forEach(function(val, index, array) {
-  if (val === '-port' && array[index + 1]) {
-    port = +array[index + 1];
+  var ex = /-c([a-zA-Z0-9]+)=(.+)/.exec(val);
+  if (ex) {
+    config[ex[1]] = ex[2];
   }
 });
 
+if (!config.port && process.env.PORT) {
+  config.port = process.env.PORT;
+}
+
+// Set config defaults
+(function() {
+  var defaults = {
+    port: 80,
+    root: ''
+  };
+  for (var def in defaults) {
+    if (defaults.hasOwnProperty(def)) {
+      if (typeof config[def] === 'undefined') {
+        config[def] = defaults[def];
+      }
+    }
+  }
+})();
+
+// Port set by:
+// - Command line: -cport=number
+// - config.json: {"port":number}
+// - Env: PORT=number node main.js
+// - Default: 80
+var port = +config.port;
+
+function getPage(pageName) {
+  var pageText = fs.readFileSync(__dirname + pageName) + '';
+  // Replace {{text}} with config values
+  pageText = pageText.replace(/\{\{([a-zA-Z0-9]+)\}\}/g, function(match, p) {
+    return config[p] || '';
+  });
+  return pageText;
+};
+
 var indexPage, singlePlayerPage, multiPlayerPage;
 
-indexPage = fs.readFileSync(__dirname + '/html/index.html');
-singlePlayerPage = fs.readFileSync(__dirname + '/html/singleplayer.html');
-multiPlayerPage = fs.readFileSync(__dirname + '/html/multiplayer.html');
+indexPage = getPage('/html/index.html');
+singlePlayerPage = getPage('/html/singleplayer.html');
+multiPlayerPage = getPage('/html/multiplayer.html');
 
 var pages = {
   'index': function(req, res) {
